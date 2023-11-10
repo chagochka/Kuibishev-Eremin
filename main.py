188dac2d06cece44003ed2c858a368dbba2e4747
chagochka <128566464+chagochka@users.noreply.github.com>
2023-11-10 17:03:57 +0700

    Обновили файлы

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication
+from UI import Ui_MainWindow
+
 from random import randint
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.paintBtn.clicked.connect(self.paint)
 
@@ -28,7 +29,7 @@ class MyWidget(QMainWindow):
 
     def draw_circle(self, qp):
         d = randint(1, self.x())
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(randint(1, self.x()), randint(1, self.y()), d, d)
 
 
